1d1910f26e5fb2dc64e45996824a4bf799b645a0
Samira <samira.hni1989@gmail.com>
2024-04-03 14:22:17 +0200

    git commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,14 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-
-    <div>App</div>
-  )
-
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/" element={<Home />} />
+            <Route path="/"  />
+            <Route path="/"  />
+            <Route path="/"  />
+            <Route path="/"  />
           </Route>
         </Routes>
       </BrowserRouter>
