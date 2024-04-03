a6394c25eacfcf3f88778d4429caee0270b7d3bf
Patrycja Wozniewicz <patrycja.wozniewicz@gmail.com>
2024-04-03 14:53:01 +0200

    context api wurde erstellen

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ParentContext } from './store/Context.jsx'
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ParentContext> 
     <App />
+    </ParentContext>
   </React.StrictMode>,
 )
