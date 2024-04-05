356fc92db724be26bdf1a54266a50cb25980239e
Hossaini <1989hossaini1@gmail.com>
2024-04-05 14:51:14 +0200

    die farbe plette als variable wurde hinzugefügt

--- src/store/Context.jsx
+++ src/store/Context.jsx
@@ -1,21 +1,11 @@
 import { createContext, useState, useContext } from "react";
 
-
-
-
-
-
-
 const Context = createContext();
 
 export function ParentContext({ children }) {
-
-
   return <Context.Provider value={{}}>{children}</Context.Provider>;
 }
 
 const Store = () => useContext(Context);
-   
-
 
 export default Store;
