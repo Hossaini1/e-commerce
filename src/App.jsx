068eff71da61b491f96185946a68cdefe21771ea
Hossaini <1989hossaini1@gmail.com>
2024-04-08 10:11:16 +0200

    added main

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter,Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Main from './components/Main'
+
 
 const App = () => {
   return (
     <>
+  
    <Header />
-   
+<Main />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}>
-            <Route path="/" />
-            <Route path="/" />
-            <Route path="/" />
-            <Route path="/" />
-          </Route>
+        
+            <Route path="/" element={<Home />} />
+            <Route path="" />
+            <Route path="" />
+            <Route path="" />
+         
         </Routes>
       </BrowserRouter>
       
