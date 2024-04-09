7ac6b6e34c32e56022a870cddb83d109520a6da3
Anastasios Aintinidis <taseos123@hotmail.com>
2024-04-09 11:32:46 +0200

    added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -25,50 +25,52 @@ export default function Carousel({ slides }) {
   };
 
   return (
-    <div className="flex justify-center m-24">
-      <div
-        className="grid bg-secondaryDark grid-cols-1 lg:grid-cols-2"
-        style={{ height: "80vh", width: "80%" }}
-      >
-        <div className="bg-secondaryDark text-primary self-center m-5">
-          <h1 className="text-2xl m-2">Big Sale 20%</h1>
-          <h3 className="text-2xl m-2">PAVAWS</h3>
-          <h5 className="text-2xl m-2">Shop</h5>
-        </div>
-        <div className="overflow-hidden relative rounded-xl shadow-2xl m-10">
-          <div
-            className={`flex transition ease-out duration-40`}
-            style={{
-              transform: `translateX(-${current * 100}%)`,
-            }}
-          >
-            {slides.map((s) => {
-              return <img src={s} />;
-            })}
+    <div className="col-span-3">
+      <div className="flex justify-center m-24">
+        <div
+          className="grid bg-secondaryDark grid-cols-1 lg:grid-cols-2"
+          style={{ height: "80vh", width: "80%" }}
+        >
+          <div className="bg-secondaryDark text-primary self-center m-5">
+            <h1 className="text-2xl m-2">Big Sale 20%</h1>
+            <h3 className="text-2xl m-2">PAVAWS</h3>
+            <h5 className="text-2xl m-2">Shop</h5>
           </div>
+          <div className="overflow-hidden relative rounded-xl shadow-2xl m-10">
+            <div
+              className={`flex transition ease-out duration-40`}
+              style={{
+                transform: `translateX(-${current * 100}%)`,
+              }}
+            >
+              {slides.map((s) => {
+                return <img src={s} />;
+              })}
+            </div>
 
-          <div className="absolute top-0 h-full w-full justify-between items-center flex text-primary px-10 text-3xl">
-            <button onClick={previousSlide}>
-              <BsFillArrowLeftCircleFill />
-            </button>
-            <button onClick={nextSlide}>
-              <BsFillArrowRightCircleFill />
-            </button>
-          </div>
-          <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
-            {slides.map((s, i) => {
-              return (
-                <div
-                  onClick={() => {
-                    setCurrent(i);
-                  }}
-                  key={"circle" + i}
-                  className={`rounded-full w-3 h-3 ${
-                    i == current ? "bg-primary" : "bg-secondary"
-                  }`}
-                ></div>
-              );
-            })}
+            <div className="absolute top-0 h-full w-full justify-between items-center flex text-primary px-10 text-3xl">
+              <button onClick={previousSlide}>
+                <BsFillArrowLeftCircleFill />
+              </button>
+              <button onClick={nextSlide}>
+                <BsFillArrowRightCircleFill />
+              </button>
+            </div>
+            <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
+              {slides.map((s, i) => {
+                return (
+                  <div
+                    onClick={() => {
+                      setCurrent(i);
+                    }}
+                    key={"circle" + i}
+                    className={`rounded-full w-3 h-3 ${
+                      i == current ? "bg-primary" : "bg-secondary"
+                    }`}
+                  ></div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,34 @@
 import { Outlet } from "react-router-dom";
-
 import Sidebar from "./Sidebar";
-
 import Store from "../store/Context";
-
-
-
-
+import Carousel from "./Carousel";
+import CarouselDunne from "./CarouselDunne";
 
 const Main = () => {
   const { handleClickToSeeMore } = Store();
 
-  return (
+  const slides = [
+    "https://i.ibb.co/ncrXc2V/1.png",
+    "https://i.ibb.co/B3s7v4h/2.png",
+    "https://i.ibb.co/XXR8kzF/3.png",
+    "https://i.ibb.co/yg7BSdM/4.png",
+  ];
 
-<>
-      <Sidebar />    
+  return (
+    <main className="grid grid-cols-4">
+      <Sidebar />
+      <Carousel slides={slides} />
+      <CarouselDunne />
 
       <Outlet />
-   
-        <button
-          className="text-center bg-tertiary text-primaryDark px-3 py-2 font-bold rounded-t-2xl "
-          onClick={handleClickToSeeMore}
-        >
-          See more
-        </button>
-    
 
-      
-    </>
+      {/* <button
+        className="text-center bg-tertiary text-primaryDark px-3 py-2 font-bold rounded-t-2xl "
+        onClick={handleClickToSeeMore}
+      >
+        See more
+      </button> */}
+    </main>
   );
 };
 export default Main;
