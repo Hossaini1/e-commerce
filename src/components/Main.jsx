a72986002e9c087cfe0646656c325a29e7488df9
Hossaini <1989hossaini1@gmail.com>
2024-04-09 10:47:39 +0200

    main tag entfernt

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
 <>
       <Sidebar />    
       <Outlet />
-      <main className="text-center ">
+      
         <button
           className="text-center bg-tertiary text-primaryDark px-3 py-2 font-bold rounded-t-2xl "
           onClick={handleClickToSeeMore}
