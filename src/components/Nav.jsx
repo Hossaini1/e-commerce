0da95dec350a4f69cd574fa1eb2d716acee7063b
kaliberda97 <kaliberdavictoria97@.gmail.com>
2024-04-10 16:05:24 +0200

    nav responsive final version

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,10 +14,6 @@ function Nav() {
     setIsActive(!isActive);
   };
 
-  const removeActive = () => {
-    setIsActive(false);
-  };
-
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
@@ -25,59 +21,50 @@ function Nav() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log('Search query:', searchQuery);
+    
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav className={`${styles.navbar}`}>
-          <img
-            src={logo}
-            alt="Logo"
-            className={`${styles.logo}`}
-            style={{ width: '170px', height: '70px', marginLeft: '100px' }}
-          />
+    <nav className={styles.navbar}>
+      <img src={logo} alt="Logo" className={styles.logo} />
 
-          <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
-            <li onClick={removeActive}>
-              <form onSubmit={handleSearchSubmit} style={{ marginRight: 'auto' }}>
-                <input
-                  type="text"
-                  value={searchQuery}
-                  onChange={handleSearchChange}
-                  placeholder="Search for anything..."
-                  className={`${styles.searchInput}`}
-                />
-              </form>
-            </li>
+      <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
+        <li>
+          <form onSubmit={handleSearchSubmit}>
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              placeholder="Search for anything ..."
+              className={styles.searchInput}
+            />
+          </form>
+        </li>
+        <div className={styles.iconContainer}>
+          <li onClick={toggleActiveClass}>
+            <div className={styles.navLink}>
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+          </li>
+          <li onClick={toggleActiveClass}>
+            <div className={styles.navLink}>
+              <FontAwesomeIcon icon={faHeart} />
+            </div>
+          </li>
+          <li onClick={toggleActiveClass}>
+            <div className={styles.navLink}>
+              <FontAwesomeIcon icon={faCartShopping} />
+            </div>
+          </li>
+        </div>
+      </ul>
 
-            <li onClick={removeActive} style={{ marginLeft: 'auto', marginRight: '-30%' }}>
-              <div className={`${styles.navLink}`}>
-                <FontAwesomeIcon icon={faUser} />
-              </div>
-            </li>
-
-            <li onClick={removeActive} style={{ marginLeft: 'auto', marginRight: '-30%' }}>
-              <div className={`${styles.navLink}`}>
-                <FontAwesomeIcon icon={faHeart} />
-              </div>
-            </li>
-
-            <li onClick={removeActive} style={{ marginLeft: 'auto', marginRight: '-30%' }}>
-              <div className={`${styles.navLink}`}>
-                <FontAwesomeIcon icon={faCartShopping} />
-              </div>
-            </li>
-          </ul>
-
-          <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`} onClick={toggleActiveClass}>
-            <span className={`${styles.bar}`}></span>
-            <span className={`${styles.bar}`}></span>
-            <span className={`${styles.bar}`}></span>
-          </div>
-        </nav>
-      </header>
-    </div>
+      <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`} onClick={toggleActiveClass}>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+      </div>
+    </nav>
   );
 }
 
