1cb029f4d20e47df3600bcbc9e49ff506a180144
kaliberda97 <kaliberdavictoria97@.gmail.com>
2024-04-16 16:32:50 +0200

    icon home hinzugefügt, search funktion erstellt

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faCartShopping, faHome } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
-
+import { Link, useNavigate } from "react-router-dom";
 
 import ShoppingCartModal from "../components/ShoppingCartModal.jsx";
 import styles from "../styles/Nav.module.css";
 import logo from "../images/haupt.png";
-import { Link } from "react-router-dom";
 
+const categoryMappings = {
+  "dress": "/dresses",
+  "skirt": "/dresses",
+  "blouse": "/blouses",
+  "t-shirt": "/blouses",
+  "pants": "/pants",
+  "jeans": "/pants",
+  "shoes": "/shoes",
+  "jacket": "/women's%20jackets",
+};
 
 function Nav() {
+  const navigate = useNavigate();
   const [isActive, setIsActive] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchCategory, setSearchCategory] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [cartItemCount, setCartItemCount] = useState(0);
 
@@ -21,17 +32,25 @@ function Nav() {
   };
 
   const handleSearchChange = (e) => {
-    setSearchQuery(e.target.value);
+    setSearchQuery(e.target.value.toLowerCase()); 
+
+    const searchCategory = categoryMappings[e.target.value.toLowerCase()];
+    setSearchCategory(searchCategory);
   };
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log("Search query:", searchQuery);
+
+    if (searchCategory) {
+      navigate(searchCategory); 
+    } else {
+      console.log("Search all categories:", searchQuery);
+    }
   };
 
   const openModal = () => {
     setIsModalOpen(true);
-    setIsActive(false); // Закрываем меню при открытии корзины
+    setIsActive(false); 
   };
 
   const closeModal = () => {
@@ -59,11 +78,17 @@ function Nav() {
 
         <div className={styles.iconContainer}>
           <li>
+            <Link to="/" className={styles.navLink}>
+              <FontAwesomeIcon icon={faHome} />
+            </Link>
+          </li>
+
+          <li>
             <Link to="/login" className={styles.navLink}>
               <FontAwesomeIcon icon={faUser} />
             </Link>
           </li>
-        
+
           <li>
             <Link to="/favorite" className={styles.navLink}>
               <FontAwesomeIcon icon={faHeart} />
